a64a59647f49d05d11c601470ff92ef72f564654
DXdok_zp32@student.itstep.org <DXdok_zp32@student.itstep.org>
2024-09-16 17:56:57 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿namespace HW_2_Dedok_interfaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Task (every three tasks in one code): ");
+            Console.ReadKey();
+            Array_2 myArray_2 = new Array_2(10);
+            myArray_2.Show();
+            myArray_2.Show("Виведення масиву:");
+            myArray_2.ShowEven();
+            myArray_2.ShowOdd();
+
+            Console.WriteLine("Кількість унікальних значень у масиві: " + myArray_2.CountDistinct());
+
+            int valueToCompare = 50;
+            Console.WriteLine($"Кількість елементів, рівних {valueToCompare}: " + myArray_2.EqualToValue(valueToCompare)); 
+
+            Console.WriteLine("Максимум: " + myArray_2.Max());
+            Console.WriteLine("Мінімум: " + myArray_2.Min());
+            Console.WriteLine("Середнє арифметичне: " + myArray_2.Avg());
+
+            int valueToSearch_1 = 50;
+            Console.WriteLine($"Шукаємо значення {valueToSearch_1} у масиві: " + (myArray_2.Search(valueToSearch_1) ? "Знайдено" : "Не знайдено"));
+
+            myArray_2.SortAsc();
+            myArray_2.Show("Масив відсортований за зростанням:");
+
+            myArray_2.SortDesc();
+            myArray_2.Show("Масив відсортований за спаданням:");
+
+            Console.WriteLine($"Кількість елементів менших за {valueToCompare}: {myArray_2.Less(valueToCompare)}");
+            Console.WriteLine($"Кількість елементів більших за {valueToCompare}: {myArray_2.Greater(valueToCompare)}");
+
+            Array_2 myArray_3 = new Array_2(10);
+            int comparisonResult = myArray_2.CompareTo(myArray_3);
+            Console.WriteLine("Порівняння двох масивів за максимальним елементом:");
+            if (comparisonResult > 0)
+            {
+                Console.WriteLine("Перший масив має більший максимум.");
+            }
+            else if (comparisonResult < 0)
+            {
+                Console.WriteLine("Другий масив має більший максимум.");
+            }
+            else
+            {
+                Console.WriteLine("Максимуми обох масивів однакові.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task_1.cs
@@ -0,0 +1,149 @@
+﻿namespace HW_2_Dedok_interfaces
+{
+    public interface IOutput_2
+    {
+        void Show();
+        void Show(string info);
+    }
+    public interface IMath_1
+    {
+        int Max();
+        int Min();
+        float Avg();
+        bool Search(int valueToSearch);
+    }
+    public interface ISort
+    {
+        void SortAsc();
+        void SortDesc();
+        void SortByParam(bool isAsc);
+    }
+    public interface ICalc
+    {
+        int Less(int valueToCompare);
+        int Greater(int valueToCompare);
+    }
+    public interface IOutput2
+    {
+        void ShowEven();
+        void ShowOdd();
+    }
+    public interface ICalc2
+    {
+        int CountDistinct();
+        int EqualToValue(int valueToCompare);
+    }
+    public class Array_2 : IOutput_2, IMath_1, ISort, ICalc, IOutput2, ICalc2, IComparable<Array_2>
+    {
+        private int[] __array;
+
+        public Array_2(int size)
+        {
+            __array = new int[size];
+            Random rand = new Random();
+            for (int i = 0; i < __array.Length; i++)
+            {
+                __array[i] = rand.Next(1, 101);
+            }
+        }
+        public void Show()
+        {
+            Console.WriteLine("Елементи масиву:");
+            foreach (int element in __array)
+            {
+                Console.Write(element + " ");
+            }
+            Console.WriteLine();
+        }
+
+        public void Show(string info)
+        {
+            Console.WriteLine(info);
+            Show();
+        }
+        public int Max()
+        {
+            return __array.Max();
+        }
+
+        public int Min()
+        {
+            return __array.Min();
+        }
+
+        public float Avg()
+        {
+            return (float)__array.Average();
+        }
+
+        public bool Search(int valueToSearch)
+        {
+            return __array.Contains(valueToSearch);
+        }
+        public void SortAsc()
+        {
+            Array.Sort(__array);
+        }
+
+        public void SortDesc()
+        {
+            Array.Sort(__array);
+            Array.Reverse(__array);
+        }
+
+        public void SortByParam(bool isAsc)
+        {
+            if (isAsc)
+            {
+                SortAsc();
+            }
+            else
+            {
+                SortDesc();
+            }
+        }
+        public int Less(int valueToCompare)
+        {
+            return __array.Count(element => element < valueToCompare);
+        }
+
+        public int Greater(int valueToCompare)
+        {
+            return __array.Count(element => element > valueToCompare);
+        }
+
+        // IOutput2 interface methods
+        public void ShowEven()
+        {
+            Console.WriteLine("Парні значення масиву:");
+            foreach (int element in __array.Where(x => x % 2 == 0))
+            {
+                Console.Write(element + " ");
+            }
+            Console.WriteLine();
+        }
+
+        public void ShowOdd()
+        {
+            Console.WriteLine("Непарні значення масиву:");
+            foreach (int element in __array.Where(x => x % 2 != 0))
+            {
+                Console.Write(element + " ");
+            }
+            Console.WriteLine();
+        }
+        public int CountDistinct()
+        {
+            return __array.Distinct().Count();
+        }
+
+        public int EqualToValue(int valueToCompare)
+        {
+            return __array.Count(x => x == valueToCompare);
+        }
+        public int CompareTo(Array_2 other)
+        {
+            return this.Max().CompareTo(other.Max());
+        }
+    }
+}
